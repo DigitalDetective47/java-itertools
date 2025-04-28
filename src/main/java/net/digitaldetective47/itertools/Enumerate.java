cb8189f742f5d89f8a8286c25bc7956779f32d87
DigitalDetective47 <ninji2701@gmail.com>
2025-04-28 16:36:38 -0400

    add Enumerate

---
+++ src/main/java/net/digitaldetective47/itertools/Enumerate.java
@@ -0,0 +1,30 @@
+package net.digitaldetective47.itertools;
+
+import java.util.AbstractMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Enumerate the elements of an iterator by the order there are returned
+ */
+public class Enumerate<T> implements Iterator<Map.Entry<Integer, T>> {
+    private Iterator<T> source;
+    private int count;
+
+    public Enumerate(Iterator<T> source, int start) {
+        this.source = source;
+        count = start;
+    }
+
+    public Enumerate(Iterator<T> source) {
+        this(source, 0);
+    }
+
+    public boolean hasNext() {
+        return source.hasNext();
+    }
+
+    public Map.Entry<Integer, T> next() {
+        return new AbstractMap.SimpleImmutableEntry<Integer, T>(count++, source.next());
+    }
+}
---
+++ src/test/java/net/digitaldetective47/itertools/EnumerateTest.java
@@ -0,0 +1,40 @@
+package net.digitaldetective47.itertools;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class EnumerateTest {
+    @Test
+    public void testDefaultStart() {
+        List<Object> objects = new ArrayList<Object>(5);
+        for (int i = 0; i < 5; i++) {
+            objects.add(new Object());
+        }
+        Enumerate<Object> stream = new Enumerate<Object>(objects.iterator());
+        for (int i = 0; i < objects.size(); i++) {
+            Map.Entry<Integer, Object> entry = stream.next();
+            assertEquals(i, entry.getKey().intValue());
+            assertSame(objects.get(i), entry.getValue());
+        }
+    }
+
+    @Test
+    public void testCustomStart() {
+        List<Object> objects = new ArrayList<Object>(5);
+        for (int i = 0; i < 5; i++) {
+            objects.add(new Object());
+        }
+        Enumerate<Object> stream = new Enumerate<Object>(objects.iterator(), 25);
+        for (int i = 0; i < objects.size(); i++) {
+            Map.Entry<Integer, Object> entry = stream.next();
+            assertEquals(i + 25, entry.getKey().intValue());
+            assertSame(objects.get(i), entry.getValue());
+        }
+    }
+}
