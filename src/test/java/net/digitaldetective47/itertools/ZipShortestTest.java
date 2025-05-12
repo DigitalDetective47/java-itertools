ca17c4809a8cd744b0409bf4e9ca88ef910bbfba
DigitalDetective47 <ninji2701@gmail.com>
2025-05-12 07:31:29 -0400

    add different size test for ZipShortest

--- src/test/java/net/digitaldetective47/itertools/ZipShortestTest.java
+++ src/test/java/net/digitaldetective47/itertools/ZipShortestTest.java
@@ -1,6 +1,7 @@
 package net.digitaldetective47.itertools;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertFalse;
 
 import java.util.Arrays;
 
@@ -16,5 +17,17 @@ public class ZipShortestTest {
         for (int i = 0; i < zipPairs.length; i++) {
             assertArrayEquals(zipPairs[i], stream.next());
         }
+    }
+
+    @Test
+    public void testDifferentSizes() {
+        Integer[] primes = { 2, 3, 5, 7, 11, 13, 17, 19};
+        Integer[] smalls = {1, 2, 3, 4, 5};
+        ZipShortest<Integer> stream = new ZipShortest<Integer>(Arrays.asList(primes), Arrays.asList(smalls));
+        Integer[][] zipPairs = {{2,1},{3,2},{5,3},{7,4},{11,5}};
+        for (int i = 0; i < zipPairs.length; i++) {
+            assertArrayEquals(zipPairs[i], stream.next());
+        }
+        assertFalse(stream.hasNext());
     }
 }
