a89cfb896e33acc577bf916325362a3500aee88e
DigitalDetective47 <ninji2701@gmail.com>
2025-06-09 13:15:49 -0400

    Accumulate now ensures its accumulation function is not null at creation time

--- src/main/java/net/digitaldetective47/itertools/Accumulate.java
+++ src/main/java/net/digitaldetective47/itertools/Accumulate.java
@@ -1,6 +1,7 @@
 package net.digitaldetective47.itertools;
 
 import java.util.Iterator;
+import java.util.Objects;
 import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
 
@@ -16,7 +17,7 @@ public class Accumulate<T, A> implements Iterator<A> {
             BiFunction<? super A, ? super T, ? extends A> accumulator) {
         sum = start;
         this.source = source.iterator();
-        this.accumulator = accumulator;
+        this.accumulator = Objects.requireNonNull(accumulator);
     }
 
     static <T> Accumulate<T, T> noStartValue(Iterable<? extends T> source, BinaryOperator<T> accumulator) {
